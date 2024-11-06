aa36488fc50ad19ec4e63c323db118786f27638c
Majekodunmi David <majekodunmidavid05@gmail.com>
2024-11-06 21:33:00 +0100

    service page added

--- app/services/page.jsx
+++ app/services/page.jsx
@@ -7,13 +7,13 @@ const services = [
   {
     num: "01",
     title: "Mobile Development",
-    description: "",
+    description: "Discover various mobile applications built using Flutter, each of the mobile application was designed with a focus on usability, performance, and seamless interaction.",
     href:""
   },
   {
     num: "02",
     title: "Web Development",
-    description: "",
+    description: "Discover web projects that demonstrate my commitment to creating responsive, engaging, and highly functional websites using React and Tailwind CSS.",
     href:""
   },
 ];
@@ -23,7 +23,7 @@ import { motion } from "framer-motion";
 const Services = () => {
   return (
     <section className="min-h-[80vh] flex flex-col justify-center py-12 xl:py-0">
-      <div className="countainer mx-auto"> 
+      <div className="countainer mx-auto px-8 lg:px-12 xl:px-24"> 
         <motion.div 
           initial={{opacity: 0}} 
           animate= {{
@@ -41,19 +41,25 @@ const Services = () => {
 
                 {/** top */}
                 <div className="w-full flex justify-between items-center">
-                  <div className="text-5xl font-extrabold text-outline text-transparent">
+                  <div className="text-5xl font-extrabold text-outline 
+                    text-transparent group-hover:text-outline-hover transition-all duration-500"
+                  >
                     {services.num}
                   </div>
-                  <Link href= {services.href}>
-                    <BsArrowDownRight />
+                  <Link href= {services.href} className="w-[70px] h-[70px] rounded-full bg-white 
+                  group-hover:bg-accent transition-all duration-500 flex justify-center items-center hover:-rotate-45">
+                    <BsArrowDownRight className="text-primary text-3xl" />
                   </Link>
                 </div>
 
                 {/** title */}
-                <h2>{services.title}</h2>
+                <h2 className="text-[42px] font-bold leading-none text-white group-hover:text-accent
+                transition-all duration-500">
+                  {services.title}
+                </h2>
                 
                 {/** description */}
-                <p>{services.description}</p>
+                <p className="text-white/60">{services.description}</p>
 
                 {/** border */}
                 <div className= "border-b border-white/20 w-full"></div>
