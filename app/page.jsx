7cb2bdef527df74f4507a37a8e82ce7b44c148f6
Majekodunmi David <majekodunmidavid05@gmail.com>
2024-11-06 19:48:45 +0100

    social link updated

--- app/page.jsx
+++ app/page.jsx
@@ -34,9 +34,9 @@ const Home = () => {
               </Button>
               <div className="mb-8 xl:mb-0">
                 <Socials containerStyles="flex gap-6" 
-                  iconStyles="w-9 h-9 border border-accent rounded-full flex 
-                  justify-center item-center text-accent text-base hover:bg-accent 
-                  hover:text-primary hover:transition-all duration-500" 
+                  iconStyles="w-10 h-10 border border-accent rounded-full flex 
+                justify-center items-center text-accent text-lg hover:bg-accent 
+                hover:text-primary transition-all duration-500" 
                 />
               </div>
             </div>
--- components/ui/Socials.jsx
+++ components/ui/Socials.jsx
@@ -1,19 +1,19 @@
-import { Link } from "lucide-react";
-import Links from "next/link";
+
+import Link from "next/link";
 
 import { FaGithub, FaLinkedin, FaInstagram } from "react-icons/fa";
 
 const socials = [
-    {icon: <FaGithub />, path: "/"},
+    {icon: <FaGithub />, path: "https://github.com/mjkdave05"},
     {icon: <FaLinkedin />, path: "https://www.linkedin.com/in/david-majekodunmi-b855a81a5/"},
-    {icon: <FaInstagram />, path: ""},
+    {icon: <FaInstagram />, path: "https://www.instagram.com/mjk_dave"},
 ]
 const Socials = ({containerStyles, iconStyles}) => {
   return (
     <div className={containerStyles}>
     {socials.map((items, index) => {
         return (
-            <Link key={index} href={items.path} className={iconStyles}>
+            <Link key={index} target="_blank" href={items.path} className={iconStyles}>
                 {items.icon}
             </Link>
         );
